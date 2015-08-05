7796ff0f690b96f49d310cbd1817dedcf1a40fad
Matthew Holt <Matthew.Holt+git@gmail.com>
2015-08-05 11:33:51 -0600

    core: Disable TLS for http sites (again)
    Fixes bug introduced in 0ac8bf5 - Also note that setup functions no longer have access to server port. Will need to fix later.

--- config/setup/tls.go
+++ config/setup/tls.go
@@ -2,7 +2,6 @@ package setup
 
 import (
 	"crypto/tls"
-	"log"
 	"strings"
 
 	"github.com/mholt/caddy/middleware"
@@ -10,11 +9,6 @@ import (
 
 func TLS(c *Controller) (middleware.Middleware, error) {
 	c.TLS.Enabled = true
-	if c.Port == "http" {
-		c.TLS.Enabled = false
-		log.Printf("Warning: TLS disabled for %s://%s. To force TLS over the plaintext HTTP port, "+
-			"specify port 80 explicitly (https://%s:80).", c.Port, c.Host, c.Host)
-	}
 
 	for c.Next() {
 		if !c.NextArg() {
