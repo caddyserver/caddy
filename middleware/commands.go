e7a4ca25c107dfda7fbcfd6f11659f44ba82911d
Dipen Patel <patelndipen@gmail.com>
2015-10-20 20:50:30 -0400

    Testing Windows's path separator modification of SplitCommandsAndArgs

--- middleware/commands.go
+++ middleware/commands.go
@@ -2,6 +2,8 @@ package middleware
 
 import (
 	"errors"
+	"runtime"
+	"strings"
 
 	"github.com/flynn/go-shlex"
 )
@@ -9,6 +11,12 @@ import (
 // SplitCommandAndArgs takes a command string and parses it
 // shell-style into the command and its separate arguments.
 func SplitCommandAndArgs(command string) (cmd string, args []string, err error) {
+
+	// ensures that shlex does not strip Windows paths of their path separator
+	if runtime.GOOS == "windows" {
+		command = strings.Replace(command, "\\", "\\\\", -1)
+	}
+
 	parts, err := shlex.Split(command)
 	if err != nil {
 		err = errors.New("error parsing command: " + err.Error())
