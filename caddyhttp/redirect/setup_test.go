b64894c31e1ecf3bb07b422e19a6e7b86ac24445
Tw <tw19881113@gmail.com>
2016-08-01 14:38:18 +0800

    redir: loading block arguments before parsing matcher
    fix issue #977
    
    Signed-off-by: Tw <tw19881113@gmail.com>

--- caddyhttp/redirect/setup_test.go
+++ caddyhttp/redirect/setup_test.go
@@ -1,6 +1,7 @@
 package redirect
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/mholt/caddy"
@@ -15,7 +16,7 @@ func TestSetup(t *testing.T) {
 		expectedRules []Rule
 	}{
 		// test case #0 tests the recognition of a valid HTTP status code defined outside of block statement
-		{"redir 300 {\n/ /foo\n}", false, []Rule{{FromPath: "/", To: "/foo", Code: 300}}},
+		{"redir 300 {\n/ /foo\n}", false, []Rule{{FromPath: "/", To: "/foo", Code: 300, RequestMatcher: httpserver.IfMatcher{}}}},
 
 		// test case #1 tests the recognition of an invalid HTTP status code defined outside of block statement
 		{"redir 9000 {\n/ /foo\n}", true, []Rule{{}}},
@@ -27,22 +28,43 @@ func TestSetup(t *testing.T) {
 		{"redir 9000 {\n/ /foo 300\n}", true, []Rule{{}}},
 
 		// test case #4 tests the recognition of a TO redirection in a block statement.The HTTP status code is set to the default of 301 - MovedPermanently
-		{"redir 302 {\n/foo\n}", false, []Rule{{FromPath: "/", To: "/foo", Code: 302}}},
+		{"redir 302 {\n/foo\n}", false, []Rule{{FromPath: "/", To: "/foo", Code: 302, RequestMatcher: httpserver.IfMatcher{}}}},
 
 		// test case #5 tests the recognition of a TO and From redirection in a block statement
-		{"redir {\n/bar /foo 303\n}", false, []Rule{{FromPath: "/bar", To: "/foo", Code: 303}}},
+		{"redir {\n/bar /foo 303\n}", false, []Rule{{FromPath: "/bar", To: "/foo", Code: 303, RequestMatcher: httpserver.IfMatcher{}}}},
 
 		// test case #6 tests the recognition of a TO redirection in a non-block statement. The HTTP status code is set to the default of 301 - MovedPermanently
-		{"redir /foo", false, []Rule{{FromPath: "/", To: "/foo", Code: 301}}},
+		{"redir /foo", false, []Rule{{FromPath: "/", To: "/foo", Code: 301, RequestMatcher: httpserver.IfMatcher{}}}},
 
 		// test case #7 tests the recognition of a TO and From redirection in a non-block statement
-		{"redir /bar /foo 303", false, []Rule{{FromPath: "/bar", To: "/foo", Code: 303}}},
+		{"redir /bar /foo 303", false, []Rule{{FromPath: "/bar", To: "/foo", Code: 303, RequestMatcher: httpserver.IfMatcher{}}}},
 
 		// test case #8 tests the recognition of multiple redirections
-		{"redir {\n / /foo 304 \n} \n redir {\n /bar /foobar 305 \n}", false, []Rule{{FromPath: "/", To: "/foo", Code: 304}, {FromPath: "/bar", To: "/foobar", Code: 305}}},
+		{"redir {\n / /foo 304 \n} \n redir {\n /bar /foobar 305 \n}", false,
+			[]Rule{{FromPath: "/", To: "/foo", Code: 304, RequestMatcher: httpserver.IfMatcher{}},
+				{FromPath: "/bar", To: "/foobar", Code: 305, RequestMatcher: httpserver.IfMatcher{}}}},
 
 		// test case #9 tests the detection of duplicate redirections
 		{"redir {\n /bar /foo 304 \n} redir {\n /bar /foo 304 \n}", true, []Rule{{}}},
+
+		// test case #10 tests the detection of a valid HTTP status code outside of a block statement being overridden by an valid HTTP status code inside statement of a block statement
+		{"redir 300 {\n/ /foo 301\n}", false, []Rule{{FromPath: "/", To: "/foo", Code: 301, RequestMatcher: httpserver.IfMatcher{}}}},
+
+		// test case #11 tests the recognition of a matcher
+		{"redir {\n if {port} is 80\n/ /foo\n}", false, []Rule{{FromPath: "/", To: "/foo", Code: 301,
+			RequestMatcher: func() httpserver.IfMatcher {
+				c := caddy.NewTestController("http", "{\n if {port} is 80\n}")
+				matcher, _ := httpserver.SetupIfMatcher(c)
+				return matcher.(httpserver.IfMatcher)
+			}()}}},
+
+		// test case #12 tests the detection of a valid HTTP status code outside of a block statement with a matcher
+		{"redir 300 {\n if {port} is 80\n/ /foo\n}", false, []Rule{{FromPath: "/", To: "/foo", Code: 300,
+			RequestMatcher: func() httpserver.IfMatcher {
+				c := caddy.NewTestController("http", "{\n if {port} is 80\n}")
+				matcher, _ := httpserver.SetupIfMatcher(c)
+				return matcher.(httpserver.IfMatcher)
+			}()}}},
 	} {
 		c := caddy.NewTestController("http", test.input)
 		err := setup(c)
@@ -63,6 +85,9 @@ func TestSetup(t *testing.T) {
 			}
 			if recievedRule.Code != test.expectedRules[i].Code {
 				t.Errorf("Test case #%d.%d expected a HTTP status code of %d, but recieved a code of %d", j, i, test.expectedRules[i].Code, recievedRule.Code)
+			}
+			if gotMatcher, expectMatcher := fmt.Sprint(recievedRule.RequestMatcher), fmt.Sprint(test.expectedRules[i].RequestMatcher); gotMatcher != expectMatcher {
+				t.Errorf("Test case #%d.%d expected a Matcher %s, but recieved a Matcher %s", j, i, expectMatcher, gotMatcher)
 			}
 		}
 	}
