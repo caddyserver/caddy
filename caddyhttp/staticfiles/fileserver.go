79760344e7b1231e59a2867246e7689b05e92e18
Echsecutor <sebastian@schmittner.pw>
2016-09-01 14:43:05 +0200

    fixing ineffectual assignments

--- caddyhttp/staticfiles/fileserver.go
+++ caddyhttp/staticfiles/fileserver.go
@@ -103,7 +103,7 @@ func (fs FileServer) serveFile(w http.ResponseWriter, r *http.Request, name stri
 				defer ff.Close()
 				dd, err := ff.Stat()
 				if err == nil {
-					name = index
+					//name = index //ineffectual
 					d = dd
 					f = ff
 					break
