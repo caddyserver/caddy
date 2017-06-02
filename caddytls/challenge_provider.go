751fcfde2d093cd54348bb08270aa2cdf94d6c1b
bamlin <b.amling@tarent.de>
2017-06-02 12:10:33 +0200

    fixed up comments for ChallengeProvider

--- caddytls/challenge_provider.go
+++ caddytls/challenge_provider.go
@@ -2,6 +2,5 @@ package caddytls
 
 import "github.com/xenolf/lego/acme"
 
-// own ChallengeProvider type to be used in Caddy plugins over acme.ChallengeProvider directly, to avoid
-// https://github.com/mattfarina/golang-broken-vendor
+// ChallengeProvider type to be used in Caddy plugins over acme.ChallengeProvider directly, to avoid https://github.com/mattfarina/golang-broken-vendor
 type ChallengeProvider acme.ChallengeProvider
