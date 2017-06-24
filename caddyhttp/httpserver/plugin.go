d3fc9f7a9bcf8272bdd0c022c149db5501ba4f92
Jason Hutchinson <zikes@zikes.me>
2017-06-24 16:58:33 -0500

    add gopkg plugin (#1725)

--- caddyhttp/httpserver/plugin.go
+++ caddyhttp/httpserver/plugin.go
@@ -494,6 +494,7 @@ var directives = []string{
 	"mailout",   // github.com/SchumacherFM/mailout
 	"awslambda", // github.com/coopernurse/caddy-awslambda
 	"grpc",      // github.com/pieterlouw/caddy-grpc
+	"gopkg",     // github.com/zikes/gopkg
 }
 
 const (
