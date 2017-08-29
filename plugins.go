08717b8575bb698256225a0d3dca9210e7b9ab67
Eldin Hadzic <eldinhadzic@protonmail.com>
2017-08-29 15:31:41 +0000

    Modify Comment

--- plugins.go
+++ plugins.go
@@ -217,7 +217,7 @@ func RegisterPlugin(name string, plugin Plugin) {
 // EventName represents the name of an event used with event hooks.
 type EventName string
 
-// Define the event names for the startup, shutdown, instancestartup and certrenew events
+// Define names for the various events
 const (
 	StartupEvent         EventName = "startup"
 	ShutdownEvent        EventName = "shutdown"
