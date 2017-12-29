4e608d348aec0b8450501a56e2013f7d067ede2f
Alfredo Uribe <alfredo@cloudspace.com>
2017-12-29 14:14:16 -0500

    changing port number for golang-auth
    at the moment we're running a refresh token server in container
    golang-auth runs on locally on por 8081 and reauth-refresh
    (in the Caddyfile) needs to point to it as part of aqfer's auth

--- aqfer/golang-auth/main/main.go
+++ aqfer/golang-auth/main/main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", Auth)
-	http.ListenAndServe(":8083", nil)
+	http.ListenAndServe(":8081", nil)
 }
 
 func Auth(w http.ResponseWriter, r *http.Request) {
