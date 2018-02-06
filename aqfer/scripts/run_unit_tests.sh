4638f256aaca90948901be3547c1393c635e05ba
Alfredo Uribe <alfredo@cloudspace.com>
2018-02-06 00:42:28 -0500

    submodule unit tests

--- aqfer/scripts/run_unit_tests.sh
+++ aqfer/scripts/run_unit_tests.sh
@@ -3,3 +3,11 @@
 
 cd /go/src/github.com/fellou89/caddy-awscloudwatch/
 go test
+cd /go/src/github.com/fellou89/caddy-cache/
+go test
+cd /go/src/github.com/fellou89/caddy-reauth/backends/refresh/
+go test
+cd /go/src/github.com/fellou89/caddy-redis/unit_tests/
+go test
+cd /go/src/github.com/fellou89/caddy-secrets/unit_tests/
+go test
