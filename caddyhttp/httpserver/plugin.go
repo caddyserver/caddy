cc133defc4058ad630ffaf611271c926f0bdfccb
Andrey Blinov <kodnaplakal@gmail.com>
2018-03-13 12:58:47 +0200

    Add Geoip plugin to httpserver/plugin.go

--- caddyhttp/httpserver/plugin.go
+++ caddyhttp/httpserver/plugin.go
@@ -544,6 +544,7 @@ var directives = []string{
 	"grpc",      // github.com/pieterlouw/caddy-grpc
 	"gopkg",     // github.com/zikes/gopkg
 	"restic",    // github.com/restic/caddy
+	"geoip",     // github.com/kodnaplakal/caddy-geoip
 }
 
 const (
