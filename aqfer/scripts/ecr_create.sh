e32bd53defa0c5f0f3db6af46084d407c2e1bda5
mithun-startsmartlabs <mithun@startsmartlabs.com>
2018-03-28 13:50:15 +0530

    Init

--- aqfer/scripts/ecr_create.sh
+++ aqfer/scripts/ecr_create.sh
@@ -2,6 +2,6 @@
 # set -e
 
 aws ecr create-repository --repository-name ecr-$1$2 1> /dev/null 2> /dev/null
-aws ecr describe-repositories --repository-name ecr-$1$2 > /tmp/ecrUri
-uri=$(cat /tmp/ecrUri | sed -E -n "s/.*repositoryUri.*\"(.*)\".*/\1/p")
+aws ecr describe-repositories --repository-name ecr-$1$2 > /tmp/ecrResources
+uri=$(cat /tmp/ecrResources | sed -E -n "s/.*repositoryArn.*\"(.*)\".*/\1/p")
 echo $uri
--- caddy/main.go
+++ caddy/main.go
@@ -1,28 +1,21 @@
-// Copyright 2015 Light Code Labs, LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-// By moving the application's package main logic into
-// a package other than main, it becomes much easier to
-// wrap caddy for custom builds that are go-gettable.
-// https://caddy.community/t/my-wish-for-0-9-go-gettable-custom-builds/59?u=matt
-
+// main.go
 package main
-
-import "github.com/mholt/caddy/caddy/caddymain"
-
+import (
+	"github.com/mholt/caddy/caddy/caddymain"
+	//_ "github.com/startsmartlabs/caddy-awscloudwatch"
+	_ "github.com/startsmartlabs/caddy-redis"
+	_ "github.com/startsmartlabs/caddy-secrets"
+	_ "github.com/startsmartlabs/caddy-transformrequest"
+	_ "github.com/startsmartlabs/caddy-transformresponse"
+	_ "github.com/fellou89/caddy-reauth"
+	"github.com/mholt/caddy/caddyhttp/httpserver"
+)
 var run = caddymain.Run // replaced for tests
-
 func main() {
+	httpserver.RegisterDevDirective("secrets", "root")
+	//httpserver.RegisterDevDirective("awscloudwatch", "root")
+	httpserver.RegisterDevDirective("transformrequest", "")
+	httpserver.RegisterDevDirective("redis", "")
+	httpserver.RegisterDevDirective("transformresponse", "")
 	run()
 }
