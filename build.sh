5d4d70fa3f0c9e149aedae75c3bfb33f36c746c5
neel-startsmartlabs <neelkanthraju@startsmartlabs.com>
2018-04-26 11:08:59 +0530

    fixes

--- build.sh
+++ build.sh
@@ -6,7 +6,6 @@ export AWS_DEFAULT_REGION=us-east-1
 
 # PROJECT, WORKSPACE are provided by jenkins
 
-#go get -u ./caddy/...
 go get -u "github.com/caddyserver/builds" "github.com/startsmartlabs/caddy-secrets" "github.com/startsmartlabs/caddy-transformrequest" "github.com/startsmartlabs/caddy-transformresponse" "github.com/startsmartlabs/caddy-redis" "github.com/startsmartlabs/caddy-awscloudwatch" "github.com/startsmartlabs/aqfer-io-custom-handler-api"
 
 export MAJOR_VERSION=`make -f MakefileCreatePreprod get_major_version`
