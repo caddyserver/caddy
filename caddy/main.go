2c6653e158fc2a2c5f06271e279bdbb5025b7a7d
Alfredo Uribe <alfredo@cloudspace.com>
2018-04-27 16:01:37 -0400

    s3 logging first commit

--- caddy/main.go
+++ caddy/main.go
@@ -5,8 +5,9 @@ import (
 	"github.com/mholt/caddy/caddy/caddymain"
 	"github.com/mholt/caddy/caddyhttp/httpserver"
 
-	_ "github.com/fellou89/caddy-reauth"
+	_ "github.com/freman/caddy-reauth"
 	_ "github.com/startsmartlabs/caddy-awscloudwatch"
+	_ "github.com/startsmartlabs/caddy-awss3"
 	_ "github.com/startsmartlabs/caddy-redis"
 	_ "github.com/startsmartlabs/caddy-secrets"
 	_ "github.com/startsmartlabs/caddy-transformrequest"
@@ -15,8 +16,10 @@ import (
 
 var run = caddymain.Run // replaced for tests
 func main() {
-	httpserver.RegisterDevDirective("awscloudwatch", "root")
+	httpserver.RegisterDevDirective("awss3", "root")
+	httpserver.RegisterDevDirective("awscloudwatch", "awss3")
 	httpserver.RegisterDevDirective("secrets", "awscloudwatch")
+
 	httpserver.RegisterDevDirective("transformrequest", "")
 	httpserver.RegisterDevDirective("redis", "")
 	httpserver.RegisterDevDirective("transformresponse", "")
