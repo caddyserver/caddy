e3ba9ffff22e081a7f503abd5f8d5f5bad444dc4
Michael Li <alimy@gility.net>
2019-02-06 01:33:52 +0800

    telemetry: Improve parsing of disabled-metrics flag (#2389)
    * optimized parse cli's disabledMetrics flag string to initTelemetry
    
    * add splitTrim to obtain string slice that not contain empty string
    
    * change TestSplitTrim error output
    
    * gofmt for run_test.go
    
    * restore name of disabledMetrics made more sense
    
    * optimized TestSplitTrim case
    
    * just update splitTrim comment to force CI restart

--- caddy/caddymain/run.go
+++ caddy/caddymain/run.go
@@ -412,13 +412,10 @@ func initTelemetry() error {
 			// mitigate disk space exhaustion at the collection endpoint
 			return fmt.Errorf("too many metrics to disable")
 		}
-		disabledMetricsSlice = strings.Split(disabledMetrics, ",")
-		for i, metric := range disabledMetricsSlice {
+		disabledMetricsSlice = splitTrim(disabledMetrics, ",")
+		for _, metric := range disabledMetricsSlice {
 			if metric == "instance_id" || metric == "timestamp" || metric == "disabled_metrics" {
 				return fmt.Errorf("instance_id, timestamp, and disabled_metrics cannot be disabled")
-			}
-			if metric == "" {
-				disabledMetricsSlice = append(disabledMetricsSlice[:i], disabledMetricsSlice[i+1:]...)
 			}
 		}
 	}
@@ -433,6 +430,27 @@ func initTelemetry() error {
 	}
 
 	return nil
+}
+
+// Split string s into all substrings separated by sep and returns a slice of
+// the substrings between those separators.
+//
+// If s does not contain sep and sep is not empty, Split returns a
+// slice of length 1 whose only element is s.
+//
+// If sep is empty, Split splits after each UTF-8 sequence. If both s
+// and sep are empty, Split returns an empty slice.
+//
+// Each item that in result is trim space and not empty string
+func splitTrim(s string, sep string) []string {
+	splitItems := strings.Split(s, sep)
+	trimItems := make([]string, 0, len(splitItems))
+	for _, item := range splitItems {
+		if item = strings.TrimSpace(item); item != "" {
+			trimItems = append(trimItems, item)
+		}
+	}
+	return trimItems
 }
 
 // LoadEnvFromFile loads additional envs if file provided and exists
