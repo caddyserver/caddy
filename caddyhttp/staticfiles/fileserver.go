33b00dc8b141e7c059a70e00b9e5827108e1aa83
Matthew Holt <mholt@users.noreply.github.com>
2019-02-16 08:22:04 -0700

    staticfiles: HEAD, not OPTIONS

--- caddyhttp/staticfiles/fileserver.go
+++ caddyhttp/staticfiles/fileserver.go
@@ -53,7 +53,7 @@ type FileServer struct {
 
 // ServeHTTP serves static files for r according to fs's configuration.
 func (fs FileServer) ServeHTTP(w http.ResponseWriter, r *http.Request) (int, error) {
-	if r.Method != "GET" && r.Method != "OPTIONS" {
+	if r.Method != "GET" && r.Method != "HEAD" {
 		return http.StatusMethodNotAllowed, nil
 	}
 	return fs.serveFile(w, r)
