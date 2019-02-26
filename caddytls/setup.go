72d0debde6bf01b5fdce0a4f3dc2b35cba28241a
Wèi Cōngruì <crvv.mail@gmail.com>
2019-02-26 09:39:30 +0800

    caddytls: add TLS 1.3 support and remove CBC ciphers (#2399)

--- caddytls/setup.go
+++ caddytls/setup.go
@@ -34,6 +34,10 @@ import (
 )
 
 func init() {
+	// opt-in TLS 1.3 for Go1.12
+	// TODO: remove this line when Go1.13 is released.
+	os.Setenv("GODEBUG", os.Getenv("GODEBUG")+",tls13=1")
+
 	caddy.RegisterPlugin("tls", caddy.Plugin{Action: setupTLS})
 
 	// ensure the default Storage implementation is plugged in
