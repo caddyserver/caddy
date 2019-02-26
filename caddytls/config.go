1d6ab2d6c55685ef2521526c2431b33b5c151cfe
Aaron <44198148+whalehub@users.noreply.github.com>
2019-02-26 08:40:50 +0100

    Change order of non-AESNI ciphers

--- caddytls/config.go
+++ caddytls/config.go
@@ -487,9 +487,9 @@ func GetSupportedCipherName(cipher uint16) (string, error) {
 
 // List of all the ciphers we want to use by default
 var defaultCiphers = []uint16{
- 	tls.TLS_AES_256_GCM_SHA384,
- 	tls.TLS_AES_128_GCM_SHA256,
-  	tls.TLS_CHACHA20_POLY1305_SHA256,
+	tls.TLS_AES_256_GCM_SHA384,
+	tls.TLS_AES_128_GCM_SHA256,
+	tls.TLS_CHACHA20_POLY1305_SHA256,
 	tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
 	tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
 	tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
@@ -500,9 +500,9 @@ var defaultCiphers = []uint16{
 
 // List of ciphers we should prefer if native AESNI support is missing
 var defaultCiphersNonAESNI = []uint16{
- 	tls.TLS_AES_256_GCM_SHA384,
- 	tls.TLS_AES_128_GCM_SHA256,
-  	tls.TLS_CHACHA20_POLY1305_SHA256,
+	tls.TLS_CHACHA20_POLY1305_SHA256,
+	tls.TLS_AES_256_GCM_SHA384,
+	tls.TLS_AES_128_GCM_SHA256,
 	tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
 	tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
 	tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
