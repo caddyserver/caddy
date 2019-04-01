fd3441c91829e03b2b9f567157323695840c6db8
u5surf <u5.horie@gmail.com>
2019-04-01 23:16:46 +0900

    Lint: fix SA6005 of staticcheck #2541

--- caddyhttp/httpserver/server.go
+++ caddyhttp/httpserver/server.go
@@ -443,7 +443,7 @@ func (s *Server) serveHTTP(w http.ResponseWriter, r *http.Request) (int, error) 
 	// sites that rely on TLS ClientAuth sharing a port with
 	// sites that do not - if mismatched, close the connection
 	if vhost.StrictHostMatching && r.TLS != nil &&
-		strings.ToLower(r.TLS.ServerName) != strings.ToLower(hostname) {
+		!strings.EqualFold(r.TLS.ServerName, hostname) {
 		r.Close = true
 		log.Printf("[ERROR] %s - strict host matching: SNI (%s) and HTTP Host (%s) values differ",
 			vhost.Addr, r.TLS.ServerName, hostname)
