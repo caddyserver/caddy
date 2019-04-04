281c2725acc34d88b2fb42c2316652180abb772e
Inconnu08 <taufiqrx8@gmail.com>
2019-04-04 21:55:40 +0600

    Lint: fix errcheck for log_test, markdown mholt#2541

--- caddyhttp/log/log_test.go
+++ caddyhttp/log/log_test.go
@@ -16,6 +16,7 @@ package log
 
 import (
 	"bytes"
+	"github.com/go-acme/lego/log"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -89,7 +90,9 @@ func TestLogRequestBody(t *testing.T) {
 		}},
 		Next: httpserver.HandlerFunc(func(w http.ResponseWriter, r *http.Request) (int, error) {
 			// drain up body
-			ioutil.ReadAll(r.Body)
+			if _, err := ioutil.ReadAll(r.Body); err != nil {
+				log.Println("[ERROR] failed to read request body: ", err)
+			}
 			return 0, nil
 		}),
 	}
@@ -153,7 +156,9 @@ func TestMultiEntries(t *testing.T) {
 		}},
 		Next: httpserver.HandlerFunc(func(w http.ResponseWriter, r *http.Request) (int, error) {
 			// drain up body
-			ioutil.ReadAll(r.Body)
+			if _, err := ioutil.ReadAll(r.Body); err != nil {
+				log.Println("[ERROR] failed to read request body: ", err)
+			}
 			return 0, nil
 		}),
 	}
