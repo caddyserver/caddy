f93dab755baa7d93b1b9a86a78d9ded848b492ba
Matthew Holt <mholt@users.noreply.github.com>
2019-06-13 10:55:25 -0600

    Update go modules

--- cmd/caddy2/main.go
+++ cmd/caddy2/main.go
@@ -8,6 +8,7 @@ import (
 	_ "github.com/caddyserver/caddy2/modules/caddyhttp/caddylog"
 	_ "github.com/caddyserver/caddy2/modules/caddyhttp/encode"
 	_ "github.com/caddyserver/caddy2/modules/caddyhttp/encode/gzip"
+	_ "github.com/caddyserver/caddy2/modules/caddyhttp/encode/zstd"
 	_ "github.com/caddyserver/caddy2/modules/caddyhttp/fileserver"
 	_ "github.com/caddyserver/caddy2/modules/caddyhttp/headers"
 	_ "github.com/caddyserver/caddy2/modules/caddyhttp/markdown"
