28e052807c2a7f12302d8670eb8c14875f1d1402
Dominik Braun <braun@sternentstehung.de>
2019-07-23 22:11:41 +0200

    removed benchmarks of old implementations

--- modules/caddyhttp/encode/encode_test.go
+++ modules/caddyhttp/encode/encode_test.go
@@ -1,53 +1,12 @@
 package encode
 
 import (
-	"bytes"
-	"encoding/json"
-	"net/http"
-	"sync"
 	"testing"
-
-	"github.com/caddyserver/caddy/v2/modules/caddyhttp"
 )
 
-// The following benchmarks represent a comparison between some
-// optimized methods and their previous implementation.
-//
-// Benchmark the old openResponseWriter implementation.
-func BenchmarkOldOpenResponseWriter(b *testing.B) {
-	oEnc := new(oldEncode)
-	for n := 0; n < b.N; n++ {
-		oEnc.openResponseWriter("test", nil)
-	}
-}
-
-// Benchmark the new openResponseWriter implementation.
-func BenchmarkNewOpenResponseWriter(b *testing.B) {
+func BenchmarkOpenResponseWriter(b *testing.B) {
 	enc := new(Encode)
 	for n := 0; n < b.N; n++ {
 		enc.openResponseWriter("test", nil)
-	}
-}
-
-// oldEncode is a copy of Encode that implements the older version
-// of the benchmarked methods, while the 'real' Encode holds the
-// newer methods. This should ensure a fair comparison.
-type oldEncode struct {
-	EncodingsRaw map[string]json.RawMessage `json:"encodings,omitempty"`
-	Prefer       []string                   `json:"prefer,omitempty"`
-	MinLength    int                        `json:"minimum_length,omitempty"`
-
-	writerPools map[string]*sync.Pool
-}
-
-// The old openResponseWriter version.
-func (oenc *oldEncode) openResponseWriter(encodingName string, w http.ResponseWriter) *responseWriter {
-	buf := bufPool.Get().(*bytes.Buffer)
-	buf.Reset()
-	return &responseWriter{
-		ResponseWriterWrapper: &caddyhttp.ResponseWriterWrapper{ResponseWriter: w},
-		encodingName:          encodingName,
-		buf:                   buf,
-		config:                nil,
 	}
 }
--- modules/caddyhttp/fileserver/browse_test.go
+++ modules/caddyhttp/fileserver/browse_test.go
@@ -1,38 +1,11 @@
 package fileserver
 
 import (
-	"bytes"
-	"encoding/json"
 	"html/template"
 	"testing"
 )
 
-// The following benchmarks represent a comparison between some
-// optimized methods and their previous implementation.
-//
-// Benchmark the old BrowseWriteJSON implementation.
-func BenchmarkOldBrowseWriteJSON(b *testing.B) {
-	ofsrv := new(oldFileServer)
-	listing := browseListing{
-		Name:           "test",
-		Path:           "test",
-		CanGoUp:        false,
-		Items:          make([]fileInfo, 100),
-		NumDirs:        42,
-		NumFiles:       420,
-		Sort:           "",
-		Order:          "",
-		ItemsLimitedTo: 42,
-	}
-	b.ResetTimer()
-
-	for n := 0; n < b.N; n++ {
-		ofsrv.browseWriteJSON(listing)
-	}
-}
-
-// Benchmark the new BrowseWriteJSON implementation.
-func BenchmarkNewBrowseWriteJSON(b *testing.B) {
+func BenchmarkBrowseWriteJSON(b *testing.B) {
 	fsrv := new(FileServer)
 	listing := browseListing{
 		Name:           "test",
@@ -52,33 +25,7 @@ func BenchmarkNewBrowseWriteJSON(b *testing.B) {
 	}
 }
 
-// Benchmark the old BrowseWriteHTML implementation.
-func BenchmarkOldBrowseWriteHTML(b *testing.B) {
-	ofsrv := new(oldFileServer)
-	ofsrv.Browse = &Browse{
-		TemplateFile: "",
-		template:     template.New("test"),
-	}
-	listing := browseListing{
-		Name:           "test",
-		Path:           "test",
-		CanGoUp:        false,
-		Items:          make([]fileInfo, 100),
-		NumDirs:        42,
-		NumFiles:       420,
-		Sort:           "",
-		Order:          "",
-		ItemsLimitedTo: 42,
-	}
-	b.ResetTimer()
-
-	for n := 0; n < b.N; n++ {
-		ofsrv.browseWriteHTML(listing)
-	}
-}
-
-// Benchmark the new BrowseWriteHTML implementation.
-func BenchmarkNewBrowseWriteHTML(b *testing.B) {
+func BenchmarkBrowseWriteHTML(b *testing.B) {
 	fsrv := new(FileServer)
 	fsrv.Browse = &Browse{
 		TemplateFile: "",
@@ -100,28 +47,4 @@ func BenchmarkNewBrowseWriteHTML(b *testing.B) {
 	for n := 0; n < b.N; n++ {
 		fsrv.browseWriteHTML(listing)
 	}
-}
-
-// oldFileServer is a copy of FileServer that implements the older
-// version of the benchmarked methods, while the 'real' FileServer
-// holds the newer methods. This should ensure a fair comparison.
-type oldFileServer struct {
-	Root       string   `json:"root,omitempty"`
-	Hide       []string `json:"hide,omitempty"`
-	IndexNames []string `json:"index_names,omitempty"`
-	Browse     *Browse  `json:"browse,omitempty"`
-}
-
-// The old browseWriteJSON version.
-func (ofsrv *oldFileServer) browseWriteJSON(listing browseListing) (*bytes.Buffer, error) {
-	buf := new(bytes.Buffer)
-	err := json.NewEncoder(buf).Encode(listing.Items)
-	return buf, err
-}
-
-// The old browseWriteHTML version.
-func (ofsrv *oldFileServer) browseWriteHTML(listing browseListing) (*bytes.Buffer, error) {
-	buf := new(bytes.Buffer)
-	err := ofsrv.Browse.template.Execute(buf, listing)
-	return buf, err
 }
