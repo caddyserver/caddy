5664ed990d2e3434e4d9fd50568e3ff2191a991d
didil <1284255+didil@users.noreply.github.com>
2019-10-04 19:14:32 +0100

    no SetKeepAlivePeriod on openbsd

--- caddyhttp/httpserver/server.go
+++ caddyhttp/httpserver/server.go
@@ -562,8 +562,11 @@ func (ln tcpKeepAliveListener) Accept() (c net.Conn, err error) {
 	if err = tc.SetKeepAlive(true); err != nil {
 		return
 	}
-	if err = tc.SetKeepAlivePeriod(3 * time.Minute); err != nil {
-		return
+	// OpenBSD has no user-settable per-socket TCP keepalive
+	if runtime.GOOS != "openbsd" {
+		if err = tc.SetKeepAlivePeriod(3 * time.Minute); err != nil {
+			return
+		}
 	}
 	return tc, nil
 }
