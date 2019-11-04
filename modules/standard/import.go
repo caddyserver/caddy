7e94c3359dbec05fb343546823b2d711c4fc7d62
Matthew Holt <mholt@users.noreply.github.com>
2019-11-04 12:13:21 -0700

    cmd: Move module imports into standard packages
    This makes it easier to make "standard" caddy builds, since you'll only
    need to add a single import to get all of Caddy's standard modules.
    
    There is a package for all of Caddy's standard modules (modules/standard)
    and a package for the HTTP app's standard modules only
    (modules/caddyhttp/standard).
    
    We still need to decide which of these, if not all of them, should be
    kept in the standard build. Those which aren't should be moved out of
    this repo. See #2780.

---
+++ modules/standard/import.go
@@ -0,0 +1,14 @@
+package standard
+
+import (
+	// standard Caddy modules
+	_ "github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
+	_ "github.com/caddyserver/caddy/v2/caddyconfig/json5"
+	_ "github.com/caddyserver/caddy/v2/caddyconfig/jsonc"
+	_ "github.com/caddyserver/caddy/v2/modules/caddyhttp/standard"
+	_ "github.com/caddyserver/caddy/v2/modules/caddytls"
+	_ "github.com/caddyserver/caddy/v2/modules/caddytls/distributedstek"
+	_ "github.com/caddyserver/caddy/v2/modules/caddytls/standardstek"
+	_ "github.com/caddyserver/caddy/v2/modules/filestorage"
+	_ "github.com/caddyserver/caddy/v2/modules/logging"
+)
