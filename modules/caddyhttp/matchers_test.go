a4b021e726c6bdebf6532c107e0adce53dc1a07c
Matthew Holt <mholt@users.noreply.github.com>
2020-01-06 09:59:35 -0700

    http: path matcher: exact match by default; substring matches (#2959)
    This is a breaking change.

--- modules/caddyhttp/matchers_test.go
+++ modules/caddyhttp/matchers_test.go
@@ -183,8 +183,18 @@ func TestPathMatcher(t *testing.T) {
 			expect: false,
 		},
 		{
+			match:  MatchPath{"/foo/bar/"},
+			input:  "/foo/bar/",
+			expect: true,
+		},
+		{
 			match:  MatchPath{"/foo/bar/", "/other"},
 			input:  "/other/",
+			expect: false,
+		},
+		{
+			match:  MatchPath{"/foo/bar/", "/other"},
+			input:  "/other",
 			expect: true,
 		},
 		{
@@ -213,23 +223,28 @@ func TestPathMatcher(t *testing.T) {
 			expect: false,
 		},
 		{
-			match:  MatchPath{"=/foo"},
-			input:  "/foo",
-			expect: true,
-		},
-		{
-			match:  MatchPath{"=/foo"},
-			input:  "/foo/bar",
-			expect: false,
-		},
-		{
-			match:  MatchPath{"=/foo"},
-			input:  "/FOO",
+			match:  MatchPath{"*substring*"},
+			input:  "/foo/substring/bar.txt",
 			expect: true,
 		},
 		{
 			match:  MatchPath{"/foo"},
+			input:  "/foo/bar",
+			expect: false,
+		},
+		{
+			match:  MatchPath{"/foo"},
+			input:  "/foo/bar",
+			expect: false,
+		},
+		{
+			match:  MatchPath{"/foo"},
 			input:  "/FOO",
+			expect: true,
+		},
+		{
+			match:  MatchPath{"/foo*"},
+			input:  "/FOOOO",
 			expect: true,
 		},
 		{
@@ -239,6 +254,10 @@ func TestPathMatcher(t *testing.T) {
 		},
 	} {
 		req := &http.Request{URL: &url.URL{Path: tc.input}}
+		repl := caddy.NewReplacer()
+		ctx := context.WithValue(req.Context(), caddy.ReplacerCtxKey, repl)
+		req = req.WithContext(ctx)
+
 		actual := tc.match.Match(req)
 		if actual != tc.expect {
 			t.Errorf("Test %d %v: Expected %t, got %t for '%s'", i, tc.match, tc.expect, actual, tc.input)
