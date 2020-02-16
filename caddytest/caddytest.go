1a9405e74f2426a17a43a234f590a113c98ff58e
Mark Sargent <99003+sarge@users.noreply.github.com>
2020-02-16 18:41:37 +1300

    show caddy json config on error

--- caddytest/caddytest.go
+++ caddytest/caddytest.go
@@ -1,9 +1,13 @@
 package caddytest
 
 import (
+	"bytes"
 	"crypto/tls"
+	"encoding/json"
 	"errors"
+	"fmt"
 	"io/ioutil"
+	"net"
 	"net/http"
 	"os"
 	"path"
@@ -13,14 +17,39 @@ import (
 	"time"
 )
 
+var internalHostnames = []string{"a.caddy.local", "b.caddy.local", "c.caddy.local"}
+
+// TestContext stores the context of the testing process
+type TestContext struct {
+	t *testing.T
+}
+
+// Complete called to clean up the request free the mutex
+func (tctx *TestContext) Complete() {
+	if tctx.t.Failed() {
+		res, err := http.Get("http://localhost:2019/config/")
+		if err != nil {
+			tctx.t.Log("unable to read the current config")
+		}
+		defer res.Body.Close()
+		body, err := ioutil.ReadAll(res.Body)
+
+		var out bytes.Buffer
+		json.Indent(&out, body, "", "  ")
+		tctx.t.Logf("----------- failed with config -----------\n%s", out.String())
+	}
+}
+
 // InitServer this will configure the server with a configurion of a specific
 // type. The configType must be either "json" or the adapter type.
-func InitServer(t *testing.T, rawConfig string, configType string) {
+func InitServer(t *testing.T, rawConfig string, configType string) TestContext {
+
+	tctx := TestContext{t: t}
 
 	err := validateTestPrerequistes()
 	if err != nil {
 		t.Skipf("skipping tests as failed integration prerequites. %s", err)
-		return
+		return tctx
 	}
 
 	rawConfig = prependCaddyFilePath(rawConfig)
@@ -30,7 +59,7 @@ func InitServer(t *testing.T, rawConfig string, configType string) {
 	req, err := http.NewRequest("POST", "http://localhost:2019/load", strings.NewReader(rawConfig))
 	if err != nil {
 		t.Errorf("failed to create request. %s", err)
-		return
+		return tctx
 	}
 
 	if configType == "json" {
@@ -42,39 +71,59 @@ func InitServer(t *testing.T, rawConfig string, configType string) {
 	res, err := client.Do(req)
 	if err != nil {
 		t.Errorf("unable to contact caddy server. %s", err)
-		return
+		return tctx
 	}
 	defer res.Body.Close()
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		t.Errorf("unable to read response. %s", err)
-		return
+		return tctx
 	}
 
 	if res.StatusCode != 200 {
 		t.Logf("failed to load config:\n status code:%d \n %s", res.StatusCode, string(body))
 		t.Fail()
 	}
+	return tctx
 }
+
+var hasValidated bool
+var arePrerequisitesValid bool
 
 func validateTestPrerequistes() error {
 
-	//TODO: hostname assumptions
-	// 127.0.0.1 a.caddy.local
-	// 127.0.0.1 b.caddy.local
-	// 127.0.0.1 c.caddy.local
+	if hasValidated {
+		if !arePrerequisitesValid {
+			return errors.New("caddy integration prerequistes failed. see first error")
+		}
+		return nil
+	}
+
+	hasValidated = true
+	arePrerequisitesValid = false
+
+	for _, host := range internalHostnames {
+		ips, err := net.LookupIP(host)
+		if err != nil {
+			return fmt.Errorf("caddy integration prerequistes failed. missing dns host:%s. %s", host, err)
+		}
+
+		if len(ips) == 1 && !ips[0].IsLoopback() {
+			return fmt.Errorf("caddy integration prerequistes failed. dns host (%s) should resovle to 127.0.0.1 found %s", host, ips[0])
+		}
+	}
 
 	// check certificates are found
 	if _, err := os.Stat(getIntegrationDir() + "/caddy.local.crt"); os.IsNotExist(err) {
 		return errors.New("caddy integration test certificates not found")
 	}
-
 	if _, err := os.Stat(getIntegrationDir() + "/caddy.local.key"); os.IsNotExist(err) {
 		return errors.New("caddy integration test certificates not found")
 	}
 
 	//TODO: assert that caddy is running
 
+	arePrerequisitesValid = true
 	return nil
 }
 
--- caddytest/integration/caddyfile_test.go
+++ caddytest/integration/caddyfile_test.go
@@ -9,7 +9,7 @@ import (
 func TestRespond(t *testing.T) {
 
 	// arrange
-	caddytest.InitServer(t, ` 
+	tctx := caddytest.InitServer(t, ` 
 	{
 		http_port     9080
 		https_port    9443
@@ -22,7 +22,8 @@ func TestRespond(t *testing.T) {
 		  body "hello from a.caddy.local"
 		}	
 	  }
-	`, "caddyfile")
+  `, "caddyfile")
+	defer tctx.Complete()
 
 	// act and assert
 	caddytest.AssertGetResponse(t, "https://a.caddy.local:9443/version", 200, "hello from a.caddy.local")
@@ -54,4 +55,73 @@ func TestRedirect(t *testing.T) {
 
 	// follow redirect
 	caddytest.AssertGetResponse(t, "https://b.caddy.local:9443/", 200, "hello from b.caddy.local")
+}
+
+func Test2Hosts(t *testing.T) {
+
+	// arrange
+	tctx := caddytest.InitServer(t, `
+	{
+		http_port     9080
+		https_port    9443
+	}
+	
+	a.caddy.local:9443 {
+    tls /caddy.local.crt /caddy.local.key {
+    }
+
+    respond /hello 200 {
+      body "hello from a.caddy.local"
+    }	
+	}
+
+	b.caddy.local:9443 {
+		tls /caddy.local.crt /caddy.local.key {
+		}
+
+		respond /hello 200 {
+		  body "hello from b.caddy.local"
+		}	
+	  }
+  `, "caddyfile")
+	defer tctx.Complete()
+
+	// act and assert
+	caddytest.AssertGetResponse(t, "https://a.caddy.local:9443/hello", 200, "hello from a.caddy.local")
+	caddytest.AssertGetResponse(t, "https://b.caddy.local:9443/hello", 200, "hello from b.caddy.local")
+}
+
+func Test2HostsAndOneStaticIP(t *testing.T) {
+
+	// arrange
+	tctx := caddytest.InitServer(t, `
+	{
+		http_port     9080
+		https_port    9443
+	}
+	
+	a.caddy.local:9443, 127.0.0.1:9080 {
+    tls /caddy.local.crt /caddy.local.key {
+    }
+
+    respond /hello 200 {
+      body "hello from a.caddy.local"
+    }	
+	}
+
+	b.caddy.local:9443 {
+		tls /caddy.local.crt /caddy.local.key {
+		}
+
+		respond /hello 200 {
+		  body "hello from b.caddy.local"
+		}	
+	  }
+  `, "caddyfile")
+	defer tctx.Complete()
+
+	// act and assert
+	caddytest.AssertGetResponse(t, "http://127.0.0.1:9080/hello", 200, "hello from a.caddy.local")
+	caddytest.AssertGetResponse(t, "https://a.caddy.local:9443/hello", 200, "hello from a.caddy.local")
+	caddytest.AssertGetResponse(t, "https://b.caddy.local:9443/hello", 200, "hello from b.caddy.local")
 }
