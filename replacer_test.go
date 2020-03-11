8f47e078e39919f9e5511f4a70063a97d5b30919
Bill Glover <bill@billglover.co.uk>
2020-03-11 21:21:41 +0000

    Add additional test case based on fuzzer feedback

--- replacer_test.go
+++ replacer_test.go
@@ -160,6 +160,10 @@ func TestReplacer(t *testing.T) {
 			input:  `{}{}{}{\\\\}\\\\`,
 			expect: `{\\\}\\\\`,
 		},
+		{
+			input:  string([]byte{0x26, 0x00, 0x83, 0x7B, 0x84, 0x07, 0x5C, 0x7D, 0x84}),
+			expect: string([]byte{0x26, 0x00, 0x83, 0x7B, 0x84, 0x07, 0x7D, 0x84}),
+		},
 	} {
 		actual := rep.ReplaceAll(tc.input, tc.empty)
 		if actual != tc.expect {
