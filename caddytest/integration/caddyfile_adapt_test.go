eb4e702799be40667f3456e79d7255f95083d0a9
Francis Lavoie <lavofr@gmail.com>
2020-05-11 21:19:40 -0400

    caddytest: Try a fix for windows

--- caddytest/integration/caddyfile_adapt_test.go
+++ caddytest/integration/caddyfile_adapt_test.go
@@ -2,6 +2,7 @@ package integration
 
 import (
 	"io/ioutil"
+	"regexp"
 	"strings"
 	"testing"
 
@@ -14,6 +15,9 @@ func TestCaddyfileAdaptToJSON(t *testing.T) {
 	if err != nil {
 		t.Errorf("failed to read caddyfile_adapt dir: %s", err)
 	}
+
+	// prep a regexp to fix strings on windows
+	winNewlines := regexp.MustCompile(`\r?\n`)
 
 	for _, f := range files {
 		if f.IsDir() {
@@ -30,6 +34,9 @@ func TestCaddyfileAdaptToJSON(t *testing.T) {
 		// split the Caddyfile (first) and JSON (second) parts
 		parts := strings.Split(string(data), "----------")
 		caddyfile, json := strings.TrimSpace(parts[0]), strings.TrimSpace(parts[1])
+
+		// replace windows newlines in the json with unix newlines
+		json = winNewlines.ReplaceAllString(json, "\n")
 
 		// run the test
 		ok := caddytest.CompareAdapt(t, caddyfile, "caddyfile", json)
