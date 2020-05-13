b83b385b012142db26005eee18f2fd4ed819d4b6
utick <123liuqingdong@163.com>
2020-05-14 00:57:11 +0800

    add CookieHashSelection for session affinity

---
+++ rand.go
@@ -0,0 +1,24 @@
+package caddy
+
+import (
+	"math/rand"
+	"time"
+)
+
+const charset = "abcdefghijklmnopqrstuvwxyz" +
+	"ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+var seededRand *rand.Rand = rand.New(
+	rand.NewSource(time.Now().UnixNano()))
+
+func RandomStringWithCharset(length int, charset string) string {
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
+	}
+	return string(b)
+}
+
+func RandomString(length int) string {
+	return RandomStringWithCharset(length, charset)
+}
