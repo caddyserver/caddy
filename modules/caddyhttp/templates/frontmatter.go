1770fc0671c0fbc6a645c17c579d84b38d10529d
Thorkild Gregersen <oganga@users.noreply.github.com>
2020-05-18 21:01:04 +0200

    templates: trim windows whitespace in SplitFrontMatter; fix #3386 (#3387)
    * add test case for SplitFrontMatter showing issue with windows newline
    
    * fix issue with windows newline when using SplitFrontMatter
    
    * Update modules/caddyhttp/templates/frontmatter.go
    
    Co-authored-by: Francis Lavoie <lavofr@gmail.com>
    
    * make it mere explicit what is trimmed from firstLine
    
    * Update modules/caddyhttp/templates/frontmatter.go
    
    Co-authored-by: Francis Lavoie <lavofr@gmail.com>
    Co-authored-by: Matt Holt <mholt@users.noreply.github.com>

--- modules/caddyhttp/templates/frontmatter.go
+++ modules/caddyhttp/templates/frontmatter.go
@@ -30,6 +30,9 @@ func extractFrontMatter(input string) (map[string]interface{}, string, error) {
 	}
 	firstLine := input[firstLineStart:firstLineEnd]
 
+	// ensure residue windows carriage return byte is removed
+	firstLine = strings.TrimSpace(firstLine)
+
 	// see what kind of front matter there is, if any
 	var closingFence string
 	var fmParser func([]byte) (map[string]interface{}, error)
