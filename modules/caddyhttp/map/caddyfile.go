1c4db130da46e2ca3884cc25d4f23f1f3f34878f
Mark Sargent <99003+sarge@users.noreply.github.com>
2020-06-15 19:09:08 +1200

    renamed mmap to maphandler

--- modules/caddyhttp/map/caddyfile.go
+++ modules/caddyhttp/map/caddyfile.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package mmap
+package maphandler
 
 import (
 	"github.com/caddyserver/caddy/v2/caddyconfig/httpcaddyfile"
