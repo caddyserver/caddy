374d22911bed2b56a85e756e3fbe0be5ef0f2091
Francis Lavoie <lavofr@gmail.com>
2020-09-02 01:01:16 -0400

    logging: Add support for parsing IP masks from strings
    wip

--- modules/logging/filters.go
+++ modules/logging/filters.go
@@ -15,7 +15,11 @@
 package logging
 
 import (
+	"encoding/json"
+	"fmt"
 	"net"
+	"strconv"
+	"unicode/utf8"
 
 	"github.com/caddyserver/caddy/v2"
 	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
@@ -59,11 +63,14 @@ func (DeleteFilter) Filter(in zapcore.Field) zapcore.Field {
 // IPMaskFilter is a Caddy log field filter that
 // masks IP addresses.
 type IPMaskFilter struct {
-	// The IPv4 range in CIDR notation.
-	IPv4CIDR int `json:"ipv4_cidr,omitempty"`
+	// The IPv4 mask, as an subnet size CIDR, or a IP + CIDR string.
+	IPv4MaskRaw json.RawMessage `json:"ipv4_cidr,omitempty"`
 
-	// The IPv6 range in CIDR notation.
-	IPv6CIDR int `json:"ipv6_cidr,omitempty"`
+	// The IPv6 mask, as an subnet size CIDR, or a IP + CIDR string.
+	IPv6MaskRaw json.RawMessage `json:"ipv6_cidr,omitempty"`
+
+	v4Mask net.IPMask
+	v6Mask net.IPMask
 }
 
 // CaddyModule returns the Caddy module information.
@@ -79,6 +86,56 @@ func (IPMaskFilter) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
 	return nil
 }
 
+// Provision parses m's IP masks, from either integers or strings.
+func (m *IPMaskFilter) Provision(ctx caddy.Context) error {
+	parseRawToMask := func(rawField json.RawMessage, bitLen int) (net.IPMask, error) {
+		if rawField == nil {
+			return nil, nil
+		}
+
+		// integers or strings are both expected to be valid utf8
+		if !utf8.Valid(rawField) {
+			return nil, fmt.Errorf("not valid UTF8")
+		}
+
+		// try to convert to an int if possible, else it's a string
+		i, err := strconv.Atoi(string(rawField))
+		if err == nil {
+			// we assume the int is a subnet size CIDR
+			// e.g. "16" being equivalent to masking the last
+			// two bytes of an ipv4 address, like "255.255.0.0"
+			return net.CIDRMask(i, bitLen), nil
+		}
+
+		// we try to parse the string as an IP CIDR,
+		// i.e. something like "192.168.0.0/16", we just
+		// care about the "16" as the mask and drop the rest
+		var s string
+		if err := json.Unmarshal(rawField, &s); err != nil {
+			return nil, err
+		}
+		_, ipNet, err := net.ParseCIDR(s)
+		if err != nil {
+			return nil, err
+		}
+		return ipNet.Mask, nil
+	}
+
+	v4Mask, err := parseRawToMask(m.IPv4MaskRaw, 32)
+	if err != nil {
+		return fmt.Errorf("parsing ipv4_cidr failed: %v", err)
+	}
+	m.v4Mask = v4Mask
+
+	v6Mask, err := parseRawToMask(m.IPv6MaskRaw, 128)
+	if err != nil {
+		return fmt.Errorf("parsing ipv6_cidr failed: %v", err)
+	}
+	m.v6Mask = v6Mask
+
+	return nil
+}
+
 // Filter filters the input field.
 func (m IPMaskFilter) Filter(in zapcore.Field) zapcore.Field {
 	host, port, err := net.SplitHostPort(in.String)
@@ -89,13 +146,10 @@ func (m IPMaskFilter) Filter(in zapcore.Field) zapcore.Field {
 	if ipAddr == nil {
 		return in
 	}
-	bitLen := 32
-	cidrPrefix := m.IPv4CIDR
+	mask := m.v4Mask
 	if ipAddr.To16() != nil {
-		bitLen = 128
-		cidrPrefix = m.IPv6CIDR
+		mask = m.v6Mask
 	}
-	mask := net.CIDRMask(cidrPrefix, bitLen)
 	masked := ipAddr.Mask(mask)
 	if port == "" {
 		in.String = masked.String()
