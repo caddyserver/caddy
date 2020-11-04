9c299dd0095a8b2e869aee961f36e1eefcf403db
Gaurav Dhameeja <gdhameeja@gmail.com>
2020-11-05 00:07:41 +0530

    httpcaddyfile: Add certificate_pem placeholder short, add to godoc (#3846)
    Co-authored-by: Matt Holt <mholt@users.noreply.github.com>
    Co-authored-by: Francis Lavoie <lavofr@gmail.com>

--- caddyconfig/httpcaddyfile/httptype.go
+++ caddyconfig/httpcaddyfile/httptype.go
@@ -99,6 +99,7 @@ func (st ServerType) Setup(inputServerBlocks []caddyfile.ServerBlock,
 		"{tls_client_issuer}", "{http.request.tls.client.issuer}",
 		"{tls_client_serial}", "{http.request.tls.client.serial}",
 		"{tls_client_subject}", "{http.request.tls.client.subject}",
+		"{tls_client_certificate_pem}", "{http.request.tls.client.certificate_pem}",
 	)
 
 	// these are placeholders that allow a user-defined final
