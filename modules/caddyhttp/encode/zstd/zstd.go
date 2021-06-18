522b1a91089264c49cf0f019b7160719b0b229ab
Klaus Post <klauspost@gmail.com>
2021-06-18 10:49:49 -0700

    encode: Tweak compression settings (#4215)
    * Tweak compression settings
    
    zstd: Limit window sizes to 128K to keep memory in control both server and client size.
    zstd: Write 0 length frames. This may be needed for compatibility.
    zstd: Create fewer encoders. Small memory improvement.
    gzip: Allow -2 (Huffman only) and -3 (stateless) compression modes.
    
    * Update modules/caddyhttp/encode/zstd/zstd.go
    
    Update docs.
    
    Co-authored-by: Francis Lavoie <lavofr@gmail.com>
    
    Co-authored-by: Francis Lavoie <lavofr@gmail.com>

--- modules/caddyhttp/encode/zstd/zstd.go
+++ modules/caddyhttp/encode/zstd/zstd.go
@@ -47,7 +47,10 @@ func (Zstd) AcceptEncoding() string { return "zstd" }
 
 // NewEncoder returns a new gzip writer.
 func (z Zstd) NewEncoder() encode.Encoder {
-	writer, _ := zstd.NewWriter(nil)
+	// The default of 8MB for the window is
+	// too large for many clients, so we limit
+	// it to 128K to lighten their load.
+	writer, _ := zstd.NewWriter(nil, zstd.WithWindowSize(128<<10), zstd.WithEncoderConcurrency(1), zstd.WithZeroFrames(true))
 	return writer
 }
 
