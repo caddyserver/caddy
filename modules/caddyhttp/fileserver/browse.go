85b62644557c31f93f2282ad0165fdfa72c99a00
HayatoShiba <55432631+HayatoShiba@users.noreply.github.com>
2021-09-18 20:51:59 +0900

    fileserver: Fix displayed file size if it is symlink (#4354)
    * Fix file size if it is symlink
    
    * change the variable name for readability

--- modules/caddyhttp/fileserver/browse.go
+++ modules/caddyhttp/fileserver/browse.go
@@ -137,7 +137,12 @@ func (fsrv *FileServer) loadDirectoryContents(dir *os.File, root, urlPath string
 	// user can presumably browse "up" to parent folder if path is longer than "/"
 	canGoUp := len(urlPath) > 1
 
-	return fsrv.directoryListing(files, canGoUp, root, urlPath, repl), nil
+	l, err := fsrv.directoryListing(files, canGoUp, root, urlPath, repl)
+	if err != nil {
+		return browseTemplateContext{}, err
+	}
+
+	return l, nil
 }
 
 // browseApplyQueryParams applies query parameters to the listing.
