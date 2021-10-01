cbb045a121464527d85cce1b56250480b0515f9a
Oleg <musinit@yandex.ru>
2021-10-02 01:27:29 +0300

    caddyhttp: Placeholder for client cert in DER + base64 format (#4241)
    * client.certificate_pem_encoded in base64 format
    
    * base64-encoding without pem encoding;naming change
    
    * fix cert.Raw instead of block.bytes

--- caddyconfig/httpcaddyfile/httptype.go
+++ caddyconfig/httpcaddyfile/httptype.go
@@ -113,6 +113,7 @@ func (st ServerType) Setup(inputServerBlocks []caddyfile.ServerBlock,
 		"{tls_client_serial}", "{http.request.tls.client.serial}",
 		"{tls_client_subject}", "{http.request.tls.client.subject}",
 		"{tls_client_certificate_pem}", "{http.request.tls.client.certificate_pem}",
+		"{tls_client_certificate_der_base64}", "{http.request.tls.client.certificate_der_base64}",
 		"{upstream_hostport}", "{http.reverse_proxy.upstream.hostport}",
 	)
 
