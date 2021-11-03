2e147e3b38c56871587c8ba5df79336a3470bb76
Andrii Kushch <andrii.kushch@instana.com>
2021-11-03 11:31:46 +0100

    update go.opentelemetry.io/otel/semconv to 1.7.0
    Co-authored-by: Dave Henderson <dhenderson@gmail.com>

--- modules/caddyhttp/opentelemetry/tracer.go
+++ modules/caddyhttp/opentelemetry/tracer.go
@@ -16,7 +16,7 @@ import (
 	"go.opentelemetry.io/otel/propagation"
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
-	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.7.0"
 	"go.opentelemetry.io/otel/trace"
 	"go.uber.org/zap"
 	"google.golang.org/grpc/credentials"
