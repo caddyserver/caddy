3ac5b197b8550a07f826235ef5f9ddd74780bd72
Kévin Dunglas <dunglas@gmail.com>
2021-11-23 10:01:43 +0100

    logging: add a filter for query parameters (#4424)
    Co-authored-by: Matt Holt <mholt@users.noreply.github.com>
    Co-authored-by: Francis Lavoie <lavofr@gmail.com>

---
+++ modules/logging/filters_test.go
@@ -0,0 +1,41 @@
+package logging
+
+import (
+	"testing"
+
+	"go.uber.org/zap/zapcore"
+)
+
+func TestQueryFilter(t *testing.T) {
+	f := QueryFilter{[]queryFilterAction{
+		{replaceAction, "foo", "REDACTED"},
+		{replaceAction, "notexist", "REDACTED"},
+		{deleteAction, "bar", ""},
+		{deleteAction, "notexist", ""},
+	}}
+
+	if f.Validate() != nil {
+		t.Fatalf("the filter must be valid")
+	}
+
+	out := f.Filter(zapcore.Field{String: "/path?foo=a&foo=b&bar=c&bar=d&baz=e"})
+	if out.String != "/path?baz=e&foo=REDACTED&foo=REDACTED" {
+		t.Fatalf("query parameters have not been filtered: %s", out.String)
+	}
+}
+
+func TestValidateQueryFilter(t *testing.T) {
+	f := QueryFilter{[]queryFilterAction{
+		{},
+	}}
+	if f.Validate() == nil {
+		t.Fatalf("empty action type must be invalid")
+	}
+
+	f = QueryFilter{[]queryFilterAction{
+		{Type: "foo"},
+	}}
+	if f.Validate() == nil {
+		t.Fatalf("unknown action type must be invalid")
+	}
+}
