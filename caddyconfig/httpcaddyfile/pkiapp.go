aaad575eb96ef31d9f8c711c858387b5b1e60d81
Francis Lavoie <lavofr@gmail.com>
2021-11-28 15:56:15 -0500

    httpcaddyfile: Support configuring `pki` app names via global options

--- caddyconfig/httpcaddyfile/pkiapp.go
+++ caddyconfig/httpcaddyfile/pkiapp.go
@@ -16,8 +16,77 @@ package httpcaddyfile
 
 import (
 	"github.com/caddyserver/caddy/v2/caddyconfig"
+	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
 	"github.com/caddyserver/caddy/v2/modules/caddypki"
 )
+
+func init() {
+	RegisterGlobalOption("pki", parsePKIApp)
+}
+
+// parsePKIApp parses the global log option. Syntax:
+//
+//     pki [id] {
+//         name                     <name>
+//         root_common_name         <name>
+//         intermediate_common_name <name>
+//     }
+//
+// When the CA ID is unspecified, 'local' is assumed.
+//
+func parsePKIApp(d *caddyfile.Dispenser, existingVal interface{}) (interface{}, error) {
+	var pki *caddypki.PKI
+	if existingVal != nil {
+		unwrappedPki, ok := existingVal.(*caddypki.PKI)
+		if !ok {
+			return nil, d.Errf("failed to unwrap existing PKI value")
+		}
+		pki = unwrappedPki
+	} else {
+		pki = &caddypki.PKI{CAs: make(map[string]*caddypki.CA)}
+	}
+
+	pkiCa := new(caddypki.CA)
+	for d.Next() {
+		if d.NextArg() {
+			pkiCa.ID = d.Val()
+			if d.NextArg() {
+				return nil, d.ArgErr()
+			}
+		}
+		for nesting := d.Nesting(); d.NextBlock(nesting); {
+			switch d.Val() {
+			case "name":
+				if !d.NextArg() {
+					return nil, d.ArgErr()
+				}
+				pkiCa.Name = d.Val()
+
+			case "root_common_name":
+				if !d.NextArg() {
+					return nil, d.ArgErr()
+				}
+				pkiCa.Name = d.Val()
+
+			case "intermediate_common_name":
+				if !d.NextArg() {
+					return nil, d.ArgErr()
+				}
+				pkiCa.Name = d.Val()
+
+			default:
+				return nil, d.Errf("unrecognized pki option '%s'", d.Val())
+			}
+		}
+	}
+	if pkiCa.ID == "" {
+		pkiCa.ID = caddypki.DefaultCAID
+	}
+
+	pki.CAs[pkiCa.ID] = pkiCa
+
+	return pki, nil
+}
 
 func (st ServerType) buildPKIApp(
 	pairings []sbAddrAssociation,
@@ -25,14 +94,28 @@ func (st ServerType) buildPKIApp(
 	warnings []caddyconfig.Warning,
 ) (*caddypki.PKI, []caddyconfig.Warning, error) {
 
-	pkiApp := &caddypki.PKI{CAs: make(map[string]*caddypki.CA)}
-
 	skipInstallTrust := false
 	if _, ok := options["skip_install_trust"]; ok {
 		skipInstallTrust = true
 	}
 	falseBool := false
 
+	// Load the PKI app configured via global options
+	var pkiApp *caddypki.PKI
+	unwrappedPki, ok := options["pki"].(*caddypki.PKI)
+	if ok {
+		pkiApp = unwrappedPki
+	} else {
+		pkiApp = &caddypki.PKI{CAs: make(map[string]*caddypki.CA)}
+	}
+	for _, ca := range pkiApp.CAs {
+		if skipInstallTrust {
+			ca.InstallTrust = &falseBool
+		}
+		pkiApp.CAs[ca.ID] = ca
+	}
+
+	// Add in the CAs configured via directives
 	for _, p := range pairings {
 		for _, sblock := range p.serverBlocks {
 			// find all the CAs that were defined and add them to the app config
@@ -42,7 +125,12 @@ func (st ServerType) buildPKIApp(
 				if skipInstallTrust {
 					ca.InstallTrust = &falseBool
 				}
-				pkiApp.CAs[ca.ID] = ca
+
+				// the CA might already exist from global options, so
+				// don't overwrite it in that case
+				if _, ok := pkiApp.CAs[ca.ID]; !ok {
+					pkiApp.CAs[ca.ID] = ca
+				}
 			}
 		}
 	}
