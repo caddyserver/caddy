d491fd6f917e80a6e085af86d813ce3617c381cb
Matthew Holt <mholt@users.noreply.github.com>
2022-04-15 10:19:03 -0600

    Fix lint by logging error

--- listen_linux.go
+++ listen_linux.go
@@ -5,6 +5,7 @@ import (
 	"net"
 	"syscall"
 
+	"go.uber.org/zap"
 	"golang.org/x/sys/unix"
 )
 
@@ -15,6 +16,12 @@ func Listen(network, addr string) (net.Listener, error) {
 
 func reusePort(network, address string, conn syscall.RawConn) error {
 	return conn.Control(func(descriptor uintptr) {
-		unix.SetsockoptInt(int(descriptor), unix.SOL_SOCKET, unix.SO_REUSEPORT, 1)
+		if err := unix.SetsockoptInt(int(descriptor), unix.SOL_SOCKET, unix.SO_REUSEPORT, 1); err != nil {
+			Log().Error("setting SO_REUSEPORT",
+				zap.String("network", network),
+				zap.String("address", address),
+				zap.Uintptr("descriptor", descriptor),
+				zap.Error(err))
+		}
 	})
 }
