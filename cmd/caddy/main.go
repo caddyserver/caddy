b32bddc308054b50e907b82398d897c8b97c302b
Weifeng Ge <lg31415@gmail.com>
2022-05-07 09:32:07 +0800

    Update main.go

--- cmd/caddy/main.go
+++ cmd/caddy/main.go
@@ -27,10 +27,10 @@
 package main
 
 import (
-	caddycmd "github.com/caddyserver/caddy/v2/cmd"
+	caddycmd "github.com/lg31415/caddy/v2/cmd"
 
 	// plug in Caddy modules here
-	_ "github.com/caddyserver/caddy/v2/modules/standard"
+	_ "github.com/lg31415/caddy/v2/modules/standard"
 )
 
 func main() {
