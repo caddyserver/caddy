2ff90c36b7e98677cd022d1a2f91fb88e1b2b499
WingLim <winglims@gmail.com>
2022-05-17 21:21:33 +0800

    feat: add windows service

--- notify/notify_windows.go
+++ notify/notify_windows.go
@@ -12,19 +12,41 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build !linux && !windows
-// +build !linux,!windows
+//go:build windows
+// +build windows
 
 package notify
 
+import "golang.org/x/sys/windows/svc"
+
+// globalStatus store windows service status, it can be
+// use to notify caddy status.
+var globalStatus chan<- svc.Status
+
+func SetGlobalStatus(status chan<- svc.Status) {
+	globalStatus = status
+}
+
 func notifyReadiness() error {
+	if globalStatus != nil {
+		globalStatus <- svc.Status{
+			State:   svc.Running,
+			Accepts: svc.AcceptStop | svc.AcceptShutdown,
+		}
+	}
 	return nil
 }
 
 func notifyReloading() error {
+	if globalStatus != nil {
+		globalStatus <- svc.Status{State: svc.StartPending}
+	}
 	return nil
 }
 
 func notifyStopping() error {
+	if globalStatus != nil {
+		globalStatus <- svc.Status{State: svc.StopPending}
+	}
 	return nil
 }
---
+++ service_windows.go
@@ -0,0 +1,55 @@
+// Copyright 2015 Matthew Holt and The Caddy Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//go:build windows
+// +build windows
+
+package caddy
+
+import (
+	"github.com/caddyserver/caddy/v2/notify"
+	"golang.org/x/sys/windows/svc"
+)
+
+func init() {
+	isService, err := svc.IsWindowsService()
+	if err != nil {
+		return
+	}
+	if isService {
+		return
+	}
+	go func() {
+		_ = svc.Run("", runner{})
+	}()
+}
+
+type runner struct{}
+
+func (runner) Execute(args []string, request <-chan svc.ChangeRequest, status chan<- svc.Status) (bool, uint32) {
+	notify.SetGlobalStatus(status)
+	status <- svc.Status{State: svc.StartPending}
+
+	for {
+		c := <-request
+		switch c.Cmd {
+		case svc.Interrogate:
+			status <- c.CurrentStatus
+		case svc.Stop, svc.Shutdown:
+			status <- svc.Status{State: svc.StopPending}
+			exitProcessFromSignal("SIGINT")
+			return false, 0
+		}
+	}
+}
