256941619caa486ce5f67b5efeaec7904059f6f5
Matthew Holt <mholt@users.noreply.github.com>
2022-08-23 22:28:11 -0600

    caddytls: Log error if ask request fails
    Errors returned from the DecisionFunc (whether to get a cert on-demand)
    are used as a signal whether to allow a cert or not; *any* error
    will forbid cert issuance.
    
    We bubble up the error all the way to the caller, but that caller is the
    Go standard library which might gobble it up.
    Now we explicitly log connection errors so sysadmins can
    ensure their ask endpoints are working.
    
    Thanks to our sponsor AppCove for reporting this!

--- modules/caddytls/automation.go
+++ modules/caddytls/automation.go
@@ -16,6 +16,7 @@ package caddytls
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
 	"time"
@@ -23,6 +24,7 @@ import (
 	"github.com/caddyserver/caddy/v2"
 	"github.com/caddyserver/certmagic"
 	"github.com/mholt/acmez"
+	"go.uber.org/zap"
 )
 
 // AutomationConfig governs the automated management of TLS certificates.
@@ -174,6 +176,13 @@ func (ap *AutomationPolicy) Provision(tlsApp *TLS) error {
 					tlsApp.Automation.OnDemand.Ask != "" {
 					err := onDemandAskRequest(tlsApp.Automation.OnDemand.Ask, name)
 					if err != nil {
+						// distinguish true errors from denials, because it's important to log actual errors
+						if !errors.Is(err, errAskDenied) {
+							tlsApp.logger.Error("request to 'ask' endpoint failed",
+								zap.Error(err),
+								zap.String("endpoint", tlsApp.Automation.OnDemand.Ask),
+								zap.String("domain", name))
+						}
 						return err
 					}
 				}
