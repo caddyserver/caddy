17f9f974f89002116b5528e1f2a3b0844bf5a3c5
Matthew Holt <mholt@users.noreply.github.com>
2022-09-13 23:57:17 -0600

    core: Wrap listeners with type that can pipe
    (Does not apply to PacketConn or quic.EarlyListener types.)

--- listen.go
+++ listen.go
@@ -15,7 +15,7 @@ import (
 func ListenTimeout(network, addr string, keepAlivePeriod time.Duration) (net.Listener, error) {
 	// check to see if plugin provides listener
 	if ln, err := getListenerFromPlugin(network, addr); err != nil || ln != nil {
-		return ln, err
+		return acceptPipe(ln), err
 	}
 
 	lnKey := listenerKey(network, addr)
@@ -29,7 +29,7 @@ func ListenTimeout(network, addr string, keepAlivePeriod time.Duration) (net.Lis
 			}
 			return nil, err
 		}
-		return &sharedListener{Listener: ln, key: lnKey}, nil
+		return &sharedListener{Listener: acceptPipe(ln), key: lnKey}, nil
 	})
 	if err != nil {
 		return nil, err
