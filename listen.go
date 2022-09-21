1426c97da57fc84d8b584a960c43fbb58df68b80
Matt Holt <mholt@users.noreply.github.com>
2022-09-21 12:55:23 -0600

    core: Reuse unix sockets (UDS) and don't try to serve HTTP/3 over UDS (#5063)
    * core: Reuse unix sockets
    
    * Don't serve HTTP/3 over unix sockets
    
    This requires upstream support, if even useful
    
    * Don't use unix build tag... yet
    
    * Fix build tag
    
    * Allow ErrNotExist when unlinking socket

--- listen.go
+++ listen.go
@@ -1,4 +1,21 @@
-//go:build !linux
+// Copyright 2015 Matthew Holt and The Caddy Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// TODO: Go 1.19 introduced the "unix" build tag. We have to support Go 1.18 until Go 1.20 is released.
+// When Go 1.19 is our minimum, change this build tag to simply "!unix".
+// (see similar change needed in listen_unix.go)
+//go:build !(aix || android || darwin || dragonfly || freebsd || hurd || illumos || ios || linux || netbsd || openbsd || solaris)
 
 package caddy
 
