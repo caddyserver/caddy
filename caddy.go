ef0b68f73a2cee2dc50d1f7ab8eb2f27a1d1064e
Adam Weinberger <adamw@adamw.org>
2022-09-23 09:05:25 -0600

    Allow version to be set manually
    When Caddy is built from a release tarball (as downloaded from GitHub),
    `caddy version` returns an empty string. This causes confusion for
    downstream packagers.
    
    With this commit, VersionString can be set with eg.
      go build (...) -ldflags '-X (...).VersionString=v1.2.3'
    Then the short form version will be "v1.2.3", and the full version
    string will begin with "v1.2.3 ".

--- caddy.go
+++ caddy.go
@@ -824,6 +824,13 @@ func InstanceID() (uuid.UUID, error) {
 	return uuid.ParseBytes(uuidFileBytes)
 }
 
+// VersionString is an optional string that overrides Caddy's
+// reported version. It can be helpful when downstream packagers
+// need to manually set Caddy's version. The short form version (see
+// Version()) will be set to VersionString, and the full version
+// will include VersionString at the beginning.
+var VersionString string
+
 // Version returns the Caddy version in a simple/short form, and
 // a full version string. The short form will not have spaces and
 // is intended for User-Agent strings and similar, but may be
@@ -841,6 +848,10 @@ func InstanceID() (uuid.UUID, error) {
 //
 // This function is experimental and subject to change or removal.
 func Version() (simple, full string) {
+	// The full version string uses VersionString as a
+	// starting point.
+	full = VersionString + " "
+
 	// the currently-recommended way to build Caddy involves
 	// building it as a dependency so we can extract version
 	// information from go.mod tooling; once the upstream
@@ -858,7 +869,8 @@ func Version() (simple, full string) {
 		}
 	}
 	if module != nil {
-		simple, full = module.Version, module.Version
+		simple = module.Version
+		full += module.Version
 		if module.Sum != "" {
 			full += " " + module.Sum
 		}
@@ -910,7 +922,12 @@ func Version() (simple, full string) {
 		}
 	}
 
-	if simple == "" || simple == "(devel)" {
+	// If VersionString is empty, full starts with a space
+	full = strings.TrimSpace(full)
+
+	if VersionString != "" {
+		simple = VersionString
+	} else if simple == "" || simple == "(devel)" {
 		simple = "unknown"
 	}
 
