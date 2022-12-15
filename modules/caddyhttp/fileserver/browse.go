caddyserver/caddy

commit c0f76e9ed482b0abde0c3d8f5e26e9f015418ca3

fileserver: Use safe redirects in file browser

modules/caddyhttp/fileserver/browse.go
@@ -67,9 +67,7 @@ func (fsrv *FileServer) serveBrowse(root, dirPath string, w http.ResponseWriter,
 	if r.URL.Path == "" || path.Base(origReq.URL.Path) == path.Base(r.URL.Path) {
 		if !strings.HasSuffix(origReq.URL.Path, "/") {
 			fsrv.logger.Debug("redirecting to trailing slash to preserve hrefs", zap.String("request_path", r.URL.Path))
-			origReq.URL.Path += "/"
-			http.Redirect(w, r, origReq.URL.String(), http.StatusMovedPermanently)
-			return nil
+			return redirect(w, r, origReq.URL.Path+"/")
 		}
 	}
 

commit cb3a35ea47b889d67071020acb2ad3f889b6cefa

Fixes a reversion in c0f76e9
which caused the query-string to be removed from the redirect.

You can test like this, with `curl`. The current caddy `v2.6.2`
drops the query-string:

```
mkdir -p /tmp/caddy/example

caddy file-server --root /tmp/caddy --browse

curl http://127.0.0.1:80/example?query=preserved
<a href="/example/">Permanent Redirect</a>
```

but with the fix, the query-string is preserved:

```
mkdir -p /tmp/caddy/example

caddy file-server --root /tmp/caddy --browse

curl http://127.0.0.1:80/example?query=preserved
<a href="/example/?query=preserved">Permanent Redirect</a>.
```

modules/caddyhttp/fileserver/browse.go
@@ -70,7 +70,8 @@ func (fsrv *FileServer) serveBrowse(root, dirPath string, w http.ResponseWriter,
 	if r.URL.Path == "" || path.Base(origReq.URL.Path) == path.Base(r.URL.Path) {
 		if !strings.HasSuffix(origReq.URL.Path, "/") {
 			fsrv.logger.Debug("redirecting to trailing slash to preserve hrefs", zap.String("request_path", r.URL.Path))
-			return redirect(w, r, origReq.URL.Path+"/")
+			origReq.URL.Path += "/"
+			return redirect(w, r, origReq.URL.String())
 		}
 	}
 