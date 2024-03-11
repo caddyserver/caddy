83f14bb9237ff40104f445158bca731b9d9e78a9
WeidiDeng <weidi_deng@icloud.com>
2024-03-11 08:52:10 +0800

    use FlushError instead of Flush

--- modules/caddyhttp/encode/encode.go
+++ modules/caddyhttp/encode/encode.go
@@ -232,18 +232,18 @@ func (enc *Encode) Match(rw *responseWriter) bool {
 	return enc.Matcher.Match(rw.statusCode, rw.Header())
 }
 
-// Flush implements http.Flusher. It delays the actual Flush of the underlying ResponseWriterWrapper
+// FlushError is an alternative Flush returning an error. It delays the actual Flush of the underlying ResponseWriterWrapper
 // until headers were written.
-func (rw *responseWriter) Flush() {
+func (rw *responseWriter) FlushError() error {
 	if !rw.wroteHeader {
 		// flushing the underlying ResponseWriter will write header and status code,
 		// but we need to delay that until we can determine if we must encode and
 		// therefore add the Content-Encoding header; this happens in the first call
 		// to rw.Write (see bug in #4314)
-		return
+		return nil
 	}
 	//nolint:bodyclose
-	http.NewResponseController(rw.ResponseWriter).Flush()
+	return http.NewResponseController(rw.ResponseWriter).Flush()
 }
 
 // Write writes to the response. If the response qualifies,
