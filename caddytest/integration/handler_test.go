c6673ad4d8c3253179d697f06aac458e48e56546
Mohammed Al Sahaf <msaa1990@gmail.com>
2024-04-18 23:31:00 +0300

    staticresp: Use the evaluated response body for sniffing JSON content-type (#6249)

--- caddytest/integration/handler_test.go
+++ caddytest/integration/handler_test.go
@@ -1,6 +1,7 @@
 package integration
 
 import (
+	"bytes"
 	"net/http"
 	"testing"
 
@@ -28,4 +29,31 @@ func TestBrowse(t *testing.T) {
 		return
 	}
 	tester.AssertResponseCode(req, 200)
+}
+
+func TestRespondWithJSON(t *testing.T) {
+	tester := caddytest.NewTester(t)
+	tester.InitServer(`
+	{
+		skip_install_trust
+		admin localhost:2999
+		http_port     9080
+		https_port    9443
+		grace_period  1ns
+	}
+	localhost {
+		respond {http.request.body}
+	}
+  `, "caddyfile")
+
+	res, _ := tester.AssertPostResponseBody("https://localhost:9443/",
+		nil,
+		bytes.NewBufferString(`{
+		"greeting": "Hello, world!"
+	}`), 200, `{
+		"greeting": "Hello, world!"
+	}`)
+	if res.Header.Get("Content-Type") != "application/json" {
+		t.Errorf("expected Content-Type to be application/json, but was %s", res.Header.Get("Content-Type"))
+	}
 }
