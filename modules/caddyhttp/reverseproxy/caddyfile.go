07c863637dff831c99d91cce129899ac8aa7b25c
Jesper Brix Rosenkilde <jbr@humppa.dk>
2024-07-12 23:01:58 +0200

    reverseproxy: Caddyfile support for health_method (#6454)
    * Add Caddyfile support of setting active health check request method
    
    * Add integration test for active health check request method

--- modules/caddyhttp/reverseproxy/caddyfile.go
+++ modules/caddyhttp/reverseproxy/caddyfile.go
@@ -79,6 +79,7 @@ func parseCaddyfile(h httpcaddyfile.Helper) (caddyhttp.MiddlewareHandler, error)
 //	    health_headers {
 //	        <field> [<values...>]
 //	    }
+//	    health_method   <value>
 //
 //	    # passive health checking
 //	    fail_duration     <duration>
@@ -386,6 +387,18 @@ func (h *Handler) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
 				h.HealthChecks.Active = new(ActiveHealthChecks)
 			}
 			h.HealthChecks.Active.Headers = healthHeaders
+
+		case "health_method":
+			if !d.NextArg() {
+				return d.ArgErr()
+			}
+			if h.HealthChecks == nil {
+				h.HealthChecks = new(HealthChecks)
+			}
+			if h.HealthChecks.Active == nil {
+				h.HealthChecks.Active = new(ActiveHealthChecks)
+			}
+			h.HealthChecks.Active.Method = d.Val()
 
 		case "health_interval":
 			if !d.NextArg() {
