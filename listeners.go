5f4614b6433b57e0289abd330c553558debeeb4d
maycxc.github.io <9441877+MayCXC@users.noreply.github.com>
2024-10-12 19:30:17 -0400

    Trim

--- listeners.go
+++ listeners.go
@@ -394,7 +394,7 @@ func SplitNetworkAddress(a string) (network, host, port string, err error) {
 		// so try removing square brackets around an IPv6 host, adding a bogus
 		// port to take advantage of standard library's robust parser, then
 		// strip the artificial port.
-		host, _, err = net.SplitHostPort(net.JoinHostPort(strings.TrimPrefix(a, "[]"), "0"))
+		host, _, err = net.SplitHostPort(net.JoinHostPort(strings.Trim(a, "[]"), "0"))
 		port = ""
 	}
 
