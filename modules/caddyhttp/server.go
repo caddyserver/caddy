ff07c57e5738201cbbd79087d56a61c2976179e3
WeidiDeng <weidi_deng@icloud.com>
2024-10-16 08:43:28 +0800

    update from upstream

--- modules/caddyhttp/server.go
+++ modules/caddyhttp/server.go
@@ -622,9 +622,6 @@ func (s *Server) serveHTTP3(addr caddy.NetworkAddress, tlsCfg *tls.Config) error
 				Tracer:   qlog.DefaultConnectionTracer,
 			},
 			IdleTimeout: time.Duration(s.IdleTimeout),
-			ConnContext: func(ctx context.Context, c quic.Connection) context.Context {
-				return context.WithValue(ctx, quicConnCtxKey, c)
-			},
 		}
 	}
 
