3aa4d56c766f533efdf6abb2f92afdd37dd1d8ee
Atakan Yenel <atakanyenel@gmail.com>
2024-10-22 16:59:10 +0000

    Move file_limit inside browse.

--- modules/caddyhttp/fileserver/caddyfile.go
+++ modules/caddyhttp/fileserver/caddyfile.go
@@ -59,7 +59,6 @@ func parseCaddyfile(h httpcaddyfile.Helper) (caddyhttp.MiddlewareHandler, error)
 //	    browse        [<template_file>]
 //	    precompressed <formats...>
 //	    status        <status>
-//	    file_limit    <limit>
 //	    disable_canonical_uris
 //	}
 //
@@ -131,6 +130,16 @@ func (fsrv *FileServer) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
 							return d.Errf("unknown sort option '%s'", dVal)
 						}
 					}
+				case "file_limit":
+					fileLimit := d.RemainingArgs()
+					if len(fileLimit) != 1 {
+						return d.Err("file_limit should have an integer value")
+					}
+					val, _ := strconv.Atoi(fileLimit[0])
+					if fsrv.Browse.FileLimit != 0 {
+						return d.Err("file_limit is already enabled")
+					}
+					fsrv.Browse.FileLimit = val
 				default:
 					return d.Errf("unknown subdirective '%s'", d.Val())
 				}
@@ -169,17 +178,6 @@ func (fsrv *FileServer) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
 			}
 			falseBool := false
 			fsrv.CanonicalURIs = &falseBool
-
-		case "file_limit":
-			fileLimit := d.RemainingArgs()
-			if len(fileLimit) != 1 {
-				return d.Err("file_limit should have an integer value")
-			}
-			val, _ := strconv.Atoi(fileLimit[0])
-			if fsrv.Browse.FileLimit != 0 {
-				return d.Err("file_limit is already enabled")
-			}
-			fsrv.Browse.FileLimit = val
 
 		case "pass_thru":
 			if d.NextArg() {
