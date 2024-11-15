33f95c9973aef114e5f36164db17c33e9a3b453d
WeidiDeng <weidi_deng@icloud.com>
2024-11-15 11:49:42 +0800

    chore: make FastAbs comment more easy to understand (#6692)

--- filepath.go
+++ filepath.go
@@ -24,6 +24,8 @@ import (
 // FastAbs is an optimized version of filepath.Abs for Unix systems,
 // since we don't expect the working directory to ever change once
 // Caddy is running. Avoid the os.Getwd() syscall overhead.
+// It's overall the same as stdlib's implementation, the difference
+// being cached working directory.
 func FastAbs(path string) (string, error) {
 	if filepath.IsAbs(path) {
 		return filepath.Clean(path), nil
