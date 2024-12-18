f0801b9da57672774351bca21997272e3a30a585
WeidiDeng <weidi_deng@icloud.com>
2024-12-18 08:07:34 +0800

    use strconv.ParseUint instead of strconv.ParseInt
    Co-authored-by: Kévin Dunglas <kevin@dunglas.fr>

--- modules/caddyhttp/reverseproxy/fastcgi/client.go
+++ modules/caddyhttp/reverseproxy/fastcgi/client.go
@@ -141,13 +141,10 @@ func (c *client) Do(p map[string]string, req io.Reader) (r io.Reader, err error)
 	// check for CONTENT_LENGTH, since the lack of it or wrong value will cause the backend to hang
 	if clStr, ok := p["CONTENT_LENGTH"]; !ok {
 		return nil, caddyhttp.Error(http.StatusLengthRequired, nil)
-	} else {
-		cl, err := strconv.ParseInt(clStr, 10, 64)
+	} else if _, err := strconv.ParseUint(clStr, 10, 64); err != nil {
 		// stdlib won't return a negative Content-Length, but we check just in case,
 		// the most likely cause is from a missing content length, which is -1
-		if err != nil || cl < 0 {
-			return nil, caddyhttp.Error(http.StatusLengthRequired, err)
-		}
+		return nil, caddyhttp.Error(http.StatusLengthRequired, err)
 	}
 
 	writer := &streamWriter{c: c}
