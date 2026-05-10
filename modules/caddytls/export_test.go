db3d33dae1223a2f577bb4af802a339c1c6ac158
Rijul-A <31570722+Rijul-A@users.noreply.github.com>
2026-05-10 10:18:16 +0000

    remove unnecessary mutex lock

--- modules/caddytls/export_test.go
+++ modules/caddytls/export_test.go
@@ -3,8 +3,6 @@ package caddytls
 // IsManaging returns true if the name is being managed by this TLS app.
 // This is for testing purposes only.
 func (t *TLS) IsManaging(name string) bool {
-	t.serverNamesMu.Lock()
-	defer t.serverNamesMu.Unlock()
 	_, ok := t.managing[name]
 	return ok
 }
