caddyserver/caddy

commit 48ce47f1d44da485fbbf6be536a0e3822763f313

reverseproxy: Use correct cases for websocket related headers (#6621)
Co-authored-by: Francis Lavoie <lavofr@gmail.com>

modules/caddyhttp/reverseproxy/reverseproxy.go
@@ -569,6 +569,30 @@ func (h *Handler) proxyLoopIteration(r *http.Request, origReq *http.Request, w h
 	return false, proxyErr
 }
 
+// Mapping of the canonical form of the headers, to the RFC 6455 form,
+// i.e. `WebSocket` with uppercase 'S'.
+var websocketHeaderMapping = map[string]string{
+	"Sec-Websocket-Accept":     "Sec-WebSocket-Accept",
+	"Sec-Websocket-Extensions": "Sec-WebSocket-Extensions",
+	"Sec-Websocket-Key":        "Sec-WebSocket-Key",
+	"Sec-Websocket-Protocol":   "Sec-WebSocket-Protocol",
+	"Sec-Websocket-Version":    "Sec-WebSocket-Version",
+}
+
+// normalizeWebsocketHeaders ensures we use the standard casing as per
+// RFC 6455, i.e. `WebSocket` with uppercase 'S'. Most servers don't
+// care about this difference (read headers case insensitively), but
+// some do, so this maximizes compatibility with upstreams.
+// See https://github.com/caddyserver/caddy/pull/6621
+func normalizeWebsocketHeaders(header http.Header) {
+	for k, rk := range websocketHeaderMapping {
+		if v, ok := header[k]; ok {
+			delete(header, k)
+			header[rk] = v
+		}
+	}
+}
+
 // prepareRequest clones req so that it can be safely modified without
 // changing the original request or introducing data races. It then
 // modifies it so that it is ready to be proxied, except for directing
@@ -655,6 +679,7 @@ func (h Handler) prepareRequest(req *http.Request, repl *caddy.Replacer) (*http.
 	if reqUpType != "" {
 		req.Header.Set("Connection", "Upgrade")
 		req.Header.Set("Upgrade", reqUpType)
+		normalizeWebsocketHeaders(req.Header)
 	}
 
 	// Set up the PROXY protocol info

modules/caddyhttp/reverseproxy/streaming.go
@@ -66,6 +66,7 @@ func (h *Handler) handleUpgradeResponse(logger *zap.Logger, wg *sync.WaitGroup, 
 	// write header first, response headers should not be counted in size
 	// like the rest of handler chain.
 	copyHeader(rw.Header(), res.Header)
+	normalizeWebsocketHeaders(rw.Header())
 	rw.WriteHeader(res.StatusCode)
 
 	logger.Debug("upgrading connection")

commit 7ee4441f9261d649eed215f27331352c1ab5a746

reverseproxy: Move websocket header normalization later (#7921)
* Move websocket header normalization later

A regression since #6621, these headers can be rewritten by cloneHeaders
and by .Add from user and transport ops. Moving this closer to where
it's round-tripped feels like the nicest solution.

I've put the harness I used to reproduce the issue at
  https://codeberg.org/nfreya/caddy-websocket-headers-repro

* Integration test for normalized websocket headers

---------

Co-authored-by: Zen Dodd <mail@steadytao.com>
Co-authored-by: Matt Holt <mholt@users.noreply.github.com>

modules/caddyhttp/reverseproxy/reverseproxy.go
@@ -692,6 +692,11 @@ func (h *Handler) proxyLoopIteration(r *http.Request, origReq *http.Request, w h
 		}
 	}
 
+	// normalize websocket headers for compatibility with older servers
+	if upgradeType(r.Header) != "" {
+		normalizeWebsocketHeaders(r.Header)
+	}
+
 	// proxy the request to that upstream
 	proxyErr = h.reverseProxy(w, r, origReq, repl, dialInfo, next)
 	if proxyErr == nil {
@@ -840,7 +845,6 @@ func (h Handler) prepareRequest(req *http.Request, repl *caddy.Replacer) (*http.
 	if reqUpgradeType != "" {
 		req.Header.Set("Connection", "Upgrade")
 		req.Header.Set("Upgrade", reqUpgradeType)
-		normalizeWebsocketHeaders(req.Header)
 	}
 
 	// Set up the PROXY protocol info

caddytest/integration/reverseproxy_test.go
@@ -1,9 +1,13 @@
 package integration
 
 import (
+	"bufio"
+	"errors"
 	"fmt"
+	"io"
 	"net"
 	"net/http"
+	"net/textproto"
 	"os"
 	"runtime"
 	"strings"
@@ -830,6 +834,103 @@ func TestReverseProxySNIPlaceHolder(t *testing.T) {
 		req.Header.Set("X-SNI", "example.com")
 		tester.AssertResponse(req, 200, "example.com")
 	}
+}
+
+func TestReverseProxyNormalizeWebSocketHeaders(t *testing.T) {
+	// 503s if it doesn't see "WebSocket" in a request
+	ln, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		t.Fatalf("failed to listen: %v", err)
+	}
+	// http.Server would canonicalize these headers, so plain tcp:
+	errc := make(chan error, 1)
+	defer func() {
+		for range errc {
+		}
+	}()
+	defer ln.Close()
+	handleConn := func(conn net.Conn) error {
+		defer conn.Close()
+		rd := textproto.NewReader(bufio.NewReader(io.LimitReader(conn, 4096)))
+		status := 503
+		for {
+			line, err := rd.ReadLine()
+			if err != nil {
+				return fmt.Errorf("failed to read line: %v", err)
+			}
+			if len(line) == 0 {
+				break
+			}
+			if strings.Contains(line, "WebSocket") {
+				status = 200
+			}
+		}
+		res := fmt.Sprintf("HTTP/1.1 %d %s\r\nConnection: close\r\n\r\n", status, http.StatusText(status))
+		if _, err = conn.Write([]byte(res)); err != nil {
+			return fmt.Errorf("failed to write: %v", err)
+		}
+		return nil
+	}
+	go func() {
+		defer close(errc)
+		for {
+			conn, err := ln.Accept()
+			if err != nil {
+				if !errors.Is(err, net.ErrClosed) {
+					errc <- fmt.Errorf("failed to accept: %v", err)
+				}
+				return
+			}
+			if err := handleConn(conn); err != nil {
+				errc <- fmt.Errorf("failed to handle: %v", err)
+			}
+		}
+	}()
+	checkServer := func() {
+		select {
+		case err := <-errc:
+			t.Fatalf("failed to serve test request: %v", err)
+		default:
+		}
+	}
+	tester := caddytest.NewTester(t)
+	tester.InitServer(fmt.Sprintf(`
+	{
+        skip_install_trust
+        local_certs
+        admin localhost:2999
+        http_port 9080
+        https_port 9443
+        grace_period 1ns
+	}
+	http://localhost:9080 {
+		reverse_proxy http://%s {
+			# test compatibility with header manipulations
+			header_up X-Add-Header "1"
+		}
+	}`, ln.Addr()), "caddyfile")
+	req, err := http.NewRequest(http.MethodGet, "http://localhost:9080/", nil)
+	if err != nil {
+		t.Fatalf("failed to create request: %v", err)
+	}
+	// Only the first WebSocket header needs the manual assignment,
+	// but mixing in .Set is likely to be more confusing.
+	req.Header["Connection"] = []string{"Upgrade"}
+	req.Header["Upgrade"] = []string{"websocket"}
+	// 1. resembling a browser via http/1.1
+	req.Header["Sec-WebSocket-Version"] = []string{"13"}
+	tester.AssertResponse(req, 200, "")
+	checkServer()
+	delete(req.Header, "Sec-WebSocket-Version")
+	// 2. canonicalized is okay
+	req.Header["Sec-Websocket-Version"] = []string{"13"}
+	tester.AssertResponse(req, 200, "")
+	checkServer()
+	delete(req.Header, "Sec-Websocket-Version")
+	// 3. unknown header doesn't receive special handling
+	req.Header["Sec-Websocket-Unnormalizedkey"] = []string{"13"}
+	tester.AssertResponse(req, 503, "")
+	checkServer()
 }
 
 func TestWeightedRoundRobinSelectionValidation(t *testing.T) {